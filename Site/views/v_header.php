a3fcd12192091b0ae6c28d7988714e34e2b4190d
vincelemb <vincent.lembo@outlook.fr>
2018-09-24 13:40:18 +0200

    accueil corrigé

--- Site/views/v_header.php
+++ Site/views/v_header.php
@@ -37,34 +37,31 @@
 		<div class="main-header">
 			<div class="nav-icon"></div>
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<a class="navbar-brand" href="#"></a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
+			<a class="navbar-brand" href="#"><img src="./assets/images/logo.svg" id="logo-header"></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+						
 
-<nav class="navbar navbar-expand-lg navbar-light">
-		<a class="navbar-brand" href="#"><img src="./assets/images/logo.svg" id="logo-header"></a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav row">
+							<li class="nav-item">
+								<a class="nav-link" href="#">Accueil</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Terreaux</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Bellecour</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Perrache</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</nav> 
 
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav row">
-			<li class="nav-item">
-				<a class="nav-link" href="#">Accueil</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Terreaux</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Bellecour</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Perrache</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Contact</a>
-			</li>
-        </ul>
-    </div>
-</nav> 
+				</div>
