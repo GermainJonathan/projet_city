efa5ec2462ec82ab11ab6f83eea7987809b793f0
vincelemb <vincent.lembo@outlook.fr>
2018-09-24 14:02:05 +0200

    border grey navbar

--- Site/views/v_header.php
+++ Site/views/v_header.php
@@ -34,34 +34,33 @@
 		<script type="text/javascript" src="<?= PATH_SCRIPTS ?>accueil.js"></script>
 	</head> 
 	<body>
-		<div class="main-header">
-			<div class="nav-icon"></div>
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+	<div class="main-header">
+		<div class="nav-icon"></div>
+
+		<nav class="navbar navbar-expand-lg navbar-light">
 			<a class="navbar-brand" href="#"><img src="./assets/images/logo.svg" id="logo-header"></a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-						
-
-					<div class="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul class="navbar-nav row">
-							<li class="nav-item">
-								<a class="nav-link" href="#">Accueil</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Terreaux</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Bellecour</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Perrache</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Contact</a>
-							</li>
-						</ul>
-					</div>
-				</nav> 
-
-				</div>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav row">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Accueil</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Terreaux</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Bellecour</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Perrache</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav> 
+	</div>
