88b853680855bda4540beb466a5b528d0bffaed8
vincelemb <vincent.lembo@outlook.fr>
2018-09-24 11:15:23 +0200

    header v2

--- Site/views/v_header.php
+++ Site/views/v_header.php
@@ -32,21 +32,27 @@
 	</head> 
 	<body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-light row">
+		<a class="navbar-brand" href="#"><img src="./assets/images/logo.png"></a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
 
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
-			<li class="nav-item active">
-				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Accueil</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="#">Link</a>
+				<a class="nav-link" href="#">Terreaux</a>
 			</li>
-			<li class="nav-item dropdown">
+			<li class="nav-item">
+				<a class="nav-link" href="#">Bellecour</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Perrache</a>
+			</li>
+			<!-- <li class="nav-item dropdown">
 				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				Dropdown
 				</a>
@@ -59,7 +65,7 @@
 			</li>
 			<li class="nav-item">
 				<a class="nav-link disabled" href="#">Disabled</a>
-			</li>
+			</li> -->
         </ul>
     </div>
 </nav> 
