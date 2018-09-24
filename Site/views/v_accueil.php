7eb23226aa87c8f3d88f8c6762279dad707234f8
Julien GS\TECHNAX <technax@technax.com>
2018-09-24 10:37:07 +0200

    :recycle: replacing parallax plugin

--- Site/views/v_accueil.php
+++ Site/views/v_accueil.php
@@ -16,7 +16,18 @@ require_once(PATH_VIEWS.'header.php');?>
 
 <!--  Zone message d'alerte -->
 <?php require_once(PATH_VIEWS.'alert.php');?>
-<div class="parallax-window" data-parallax="scroll" data-image-src="./assets/images/lyon.jpg" style="min-height: 600px;background: transparent;"></div>
+
+<div class="parallax" id="parallax-2" data-image-src="./assets/images/lyon.jpg" data-height="650px">
+    <div class="caption">
+        <span class="border">Presqu'île</span>
+    </div>
+</div>
+
+ <div class="texttest">
+    <h3>Parallax Demo</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus felis at congue tempus. Integer egestas vehicula orci, sodales vulputate diam sodales nec. Vivamus sagittis orci id leo gravida, luctus posuere neque tincidunt. Praesent facilisis iaculis pulvinar. In porta pulvinar mi, eget congue nulla laoreet vitae. Ut vestibulum vel lorem et facilisis. In malesuada ornare enim eget tempor. In vitae ipsum odio. Cras sodales mauris non augue tincidunt porttitor. Nam ultricies consectetur quam eget fermentum. </p>
+</div>
+
 <div class="row no-padding">
     <div id="mapid"></div>
 </div>
