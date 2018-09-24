e5f519147e7816d3814d320661f1199f518492cb
Paul <paulpoupet@atilog.com>
2018-09-24 10:36:52 +0200

    ménage

--- Site/controllers/c_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once(PATH_MODELS.'ConnexionDAO.php');
-
-$connexion = new ConnexionDAO(DEBUG);
-
-$utilisateur = $connexion -> getInformationsConnexion($_SESSION['ID']);
-
-require_once(PATH_VIEWS.'user.php');
