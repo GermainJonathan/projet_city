e51e0df6f3567a9e449ed2b63b58109a01a949c4
Paul <paulpoupet@atilog.com>
2018-09-25 16:28:59 +0200

    fix controleur terreaux

--- Site/views/v_404.php
+++ Site/views/v_404.php
@@ -1,19 +1,7 @@
 <?php
-/*
- * TP PHP
- * Vue page 404
- *
- * Copyright 2016, Eric Dufour
- * http://techfacile.fr
- *
- *
- * Licensed under the MIT license:
- * http://www.opensource.org/licenses/MIT
- *
- */
-?>
-<?php require_once(PATH_VIEWS.'header.php');?>
-<?php 
-  require_once(PATH_VIEWS.'alert.php');
-?>
-<?php require_once(PATH_VIEWS.'footer.php'); 
+
+require_once PATH_VIEWS.'header.php';
+
+require_once PATH_VIEWS.'alert.php';
+
+require_once PATH_VIEWS.'footer.php';
