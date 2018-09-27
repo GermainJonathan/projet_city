7e10fa36a0547eeb914c68d942cc2432a604403e
Julien GS\TECHNAX <technax@technax.com>
2018-09-27 10:08:28 +0200

    :rocket: accueil - navbar items are dynamic

--- Site/assets/scripts/accueil.js
+++ Site/assets/scripts/accueil.js
@@ -1,13 +1,39 @@
 jQuery(document).ready(function () {
 
+    //INIT
     setResize($('#mapid'));
-    
+
+    //active selected nav-item
+    $('.nav-item').each(function () {
+        $(this).find('a[href*=' + $_GET('page') + ']').parent()
+            .toggleClass("nav-item").toggleClass("nav-item-active");
+    });
+
     $(window).resize(function () {
         setResize($('#mapid'));
     })
 
-    function setResize(element){
+    function setResize(element) {
         var imageHeight = $(window).height() * element.data('height') / 100
         element.css('height', imageHeight)
+    }
+
+    /**
+     * return get php param
+     * @param {string} param 
+     */
+    function $_GET(param) {
+        var vars = {};
+        window.location.href.replace(location.hash, '').replace(
+            /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+            function (m, key, value) { // callback
+                vars[key] = value !== undefined ? value : '';
+            }
+        );
+
+        if (param) {
+            return vars[param] ? vars[param] : null;
+        }
+        return vars;
     }
 });
