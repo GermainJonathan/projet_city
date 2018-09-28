a76686de653a958804e1533110a8e9e41fb09382
vincelemb <vincent.lembo@outlook.fr>
2018-09-28 16:24:06 +0200

    Formulaire

--- Site/views/v_header.php
+++ Site/views/v_header.php
@@ -13,6 +13,7 @@
 		<link href="<?= PATH_CSS ?>communTheme.css" rel="stylesheet"/>
 		<link href="<?= PATH_CSS ?>header.css" rel="stylesheet"/>
 		<link href="<?= PATH_CSS ?>footer.css" rel="stylesheet"/>
+		<link href="<?= PATH_CSS ?>contact.css" rel="stylesheet"/>
 		<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin=""/>
 		   
 		<script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js" integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA==" crossorigin=""></script> 
