725243dc5bdf41d1a88d8421f72357456fe631fe
GermainJonathan <jgermain@gismartware.com>
2018-10-22 13:53:00 +0200

    :iphone: Forum/Projet - Style primaire sur forum et Style global

--- Site/views/v_topic.php
+++ Site/views/v_topic.php
@@ -5,13 +5,25 @@ require_once(PATH_VIEWS.'header.php');
 
 // menu navigation
 require_once(PATH_VIEWS.'alert.php');?>
-
-<?php
-
-foreach ($listMessage as $message)
-    echo $message;
-?>
-
+<div class="bodyForum">
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Messages</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <?php
+                foreach ($listMessage as $message)
+                    echo $message;
+                ?>
+            </td>
+        </th>
+    </tbody>
+</table>
+</div>
 
 <!--  Pied de page -->
 <?php require_once(PATH_VIEWS.'footer.php');
