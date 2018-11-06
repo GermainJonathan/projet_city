03cd1bf62a2077a62d6130aca57fd9c13d63dcbb
GermainJonathan <jgermain@gismartware.com>
2018-11-06 18:33:37 +0100

    :sparkles: Menu - Ajout du boutton langue

--- Site/views/v_connexion.php
+++ Site/views/v_connexion.php
@@ -6,14 +6,22 @@ require_once(PATH_VIEWS.'header.php');
 // menu navigation
 require_once(PATH_VIEWS.'alert.php');?>
 
-<div class="form-control">
-    <form action="" method="post" name="formConnexion" id="formConnexion">
-        <label for="login">idantifiant</label>
-        <input type="text" name="login" id="login" placeholder="BruceWayne">
-        <label for="passWord">Mot de passe</label>
-        <input type="password" name="passWord" id="passWord" placeholder="B@tm4n">
-        <button form="formConnexion" type="submit" name="valFormConnexion">Valider</button>
-    </form>
+<div class="login-form container">
+    <div class="login-form-group">
+        <form action="" method="post" name="formConnexion" id="formConnexion">
+            <div class="id row col-12">
+                <label for="login">Identifiant</label>
+                <input type="text" class="form-control" name="login" id="login" placeholder="Nom d'utilisateur">
+            </div>      
+            <div clas="mdp row col-12">
+                <label for="passWord">Mot de passe</label>
+                <input type="password" class="form-control" name="passWord" id="passWord">
+            </div>
+            <div class="row col-6">
+                <button form="formConnexion" class="btn btn-primary" type="submit" name="valFormConnexion">Valider</button>
+            </div>
+        </form>
+    </div>
 </div>
 
 <!--  Pied de page -->
