324bcaca1d06e1c0c80507ad81e8aa804ac780bb
GermainJonathan <jgermain@gismartware.com>
2018-11-06 22:10:00 +0100

    :lipstick: Login - page WIP

--- Site/views/v_connexion.php
+++ Site/views/v_connexion.php
@@ -6,18 +6,27 @@ require_once(PATH_VIEWS.'header.php');
 // menu navigation
 require_once(PATH_VIEWS.'alert.php');?>
 
-<div class="login-form container">
+<div class="login-form">
     <div class="login-form-group">
-        <form action="" method="post" name="formConnexion" id="formConnexion">
-            <div class="id row col-12">
-                <label for="login">Identifiant</label>
-                <input type="text" class="form-control" name="login" id="login" placeholder="Nom d'utilisateur">
-            </div>      
-            <div clas="mdp row col-12">
-                <label for="passWord">Mot de passe</label>
-                <input type="password" class="form-control" name="passWord" id="passWord">
+        <form action="" method="post" name="formConnexion" id="formConnexion"> 
+            <div class="loginTitle no-margin row">
+                <h2>Identification</h2>
             </div>
-            <div class="row col-6">
+            <hr>
+            <div class="input-group no-margin row col-12">
+                <div class="input-group-prepend">
+                    <span class="input-group-text user" id="basic-addon1"></span>
+                </div>
+                <input type="text" name="login" id="login" class="form-control" placeholder="Nom d'utilisateur" aria-label="Username" aria-describedby="basic-addon1">
+            </div>   
+            <div class="input-group no-margin row col-12">
+                <div class="input-group-prepend">
+                    <span class="input-group-text mdp" id="basic-addon2"></span>
+                </div>
+                <input type="password" class="form-control" name="passWord" id="passWord" aria-label="Username" aria-describedby="basic-addon2">
+            </div>
+            <hr>
+            <div class="no-margin row col-6">
                 <button form="formConnexion" class="btn btn-primary" type="submit" name="valFormConnexion">Valider</button>
             </div>
         </form>
