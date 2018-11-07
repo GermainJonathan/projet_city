482354dd30c8ad8953c8f0d19407eacd14ef43a1
GermainJonathan <jgermain@gismartware.com>
2018-11-07 15:04:46 +0100

    :sparkles: Login - Page Login

--- Site/views/v_connexion.php
+++ Site/views/v_connexion.php
@@ -5,14 +5,15 @@ require_once(PATH_VIEWS.'header.php');
 
 // menu navigation
 require_once(PATH_VIEWS.'alert.php');?>
-
-<div class="login-form">
+<div class="containerLogin">
+    <div class="login-form">
+    </div>
     <div class="login-form-group">
         <form action="" method="post" name="formConnexion" id="formConnexion"> 
             <div class="loginTitle justify-content-center no-margin row">
                 <h2>Identification</h2>
             </div>
-            <hr>
+            <hr id="loginHr">
             <div class="input-group no-margin row col-12">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="basic-addon1"><i class="user"></i></span>
@@ -26,12 +27,13 @@ require_once(PATH_VIEWS.'alert.php');?>
                 <input type="password" class="form-control" name="passWord" id="passWord" aria-label="Username" aria-describedby="basic-addon2" placeholder="Mot de passe">
             </div>
             <hr>
-            <div class="no-margin justify-content-center col-offset-6 col-6">
-                <button form="formConnexion" class="btn btn-primary" type="submit" name="valFormConnexion">Valider</button>
+            <div class="row justify-content-between no-margin">
+                <div class="col-6"></div>
+                <div class="col-6">
+                    <button form="formConnexion" class="btn btn-primary" type="submit" name="valFormConnexion" id="valFormConnexion">Valider</button>
+                </div>
             </div>
         </form>
     </div>
 </div>
-
-<!--  Pied de page -->
-<?php require_once(PATH_VIEWS.'footer.php');
+<script type="text/javascript" src="<?= PATH_SCRIPTS ?>login.js"></script>
