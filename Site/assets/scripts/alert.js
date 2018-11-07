6e617ff05b2e5c7261c2c85f62ee6d0c3d8f960e
GermainJonathan <jgermain@gismartware.com>
2018-11-07 17:33:21 +0100

    :bug: Alert - Correction sur le pop des toasts

--- Site/assets/scripts/alert.js
+++ Site/assets/scripts/alert.js
@@ -1,37 +1,45 @@
 $(".alert.alert-danger").ready(function() {
-    message = $(".alert.alert-danger").children().text();
-    $('.alert.alert-danger').remove();
-    $.notify({
-        message: message
-    }, {
-        type:'danger',
-        delay: 0
-    });
+    if($(".alert.alert-danger").length) {
+        message = $(".alert.alert-danger").children().text();
+        $('.alert.alert-danger').remove();
+        $.notify({
+            message: message
+        }, {
+            type:'danger',
+            delay: 0
+        });
+    }
 });
 $(".alert.alert-warning").ready(function() {
-    message = $(".alert.alert-warning").children().text();
-    $('.alert.alert-warning').remove();
-    $.notify({
-        message: message
-    }, {
-        type:'warning'
-    });
+    if($(".alert.alert-warning").length) {
+        message = $(".alert.alert-warning").children().text();
+        $('.alert.alert-warning').remove();
+        $.notify({
+            message: message
+        }, {
+            type:'warning'
+        });
+    }
 });
 $(".alert.alert-info").ready(function() {
-    message = $(".alert.alert-info").children().text();
-    $('.alert.alert-info').remove();
-    $.notify({
-        message: message
-    }, {
-        type:'info'
-    });
+    if($(".alert.alert-info").length) {
+        message = $(".alert.alert-info").children().text();
+        $('.alert.alert-info').remove();
+        $.notify({
+            message: message
+        }, {
+            type:'info'
+        });
+    }
 });
 $(".alert.alert-success").ready(function() {
-    message = $(".alert.alert-success").children().text();
-    $('.alert.alert-success').remove();
-    $.notify({
-        message: message
-    }, {
-        type:'success'
-    });
+    if($(".alert.alert-success").length) {
+        message = $(".alert.alert-success").children().text();
+        $('.alert.alert-success').remove();
+        $.notify({
+            message: message
+        }, {
+            type:'success'
+        });
+    }
 });
