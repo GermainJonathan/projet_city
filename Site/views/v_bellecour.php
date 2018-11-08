7bdb613cc4760837ef7fd9a8d6a536fc84680930
Julien Giraud-sauveur <>
2018-11-08 09:30:57 +0100

    :art: Bellecour - merge

--- Site/views/v_bellecour.php
+++ Site/views/v_bellecour.php
@@ -10,33 +10,43 @@ require_once(PATH_VIEWS.'spinner.php');?>
 
 
 <!-- SideBar Page -->
-<div class="container-fluid">
+<div>
     <div id="sideBar">
         <nav id="menuQuartier">
             <ul class="nav navbar-nav">
                 <li>
                     <a class="nav-link" href="#anchorBodyHistoire">
-                        <p class="navLien"><?= TITRE_HISTOIRE ?></p>
+                        <p class="navLien">
+                            <?= TITRE_HISTOIRE ?>
+                        </p>
                     </a>
                 </li>
                 <li>
                     <a class="nav-link" href="#anchorBodyMonuments">
-                    <p class="navLien"><?= TITRE_MONUMENT ?></p>
+                        <p class="navLien">
+                            <?= TITRE_MONUMENT ?>
+                        </p>
                     </a>
                 </li>
                 <li>
                     <a class="nav-link" href="#anchorBodyActivites">
-                    <p class="navLien"><?= TITRE_ACTIVITE ?></p>
+                        <p class="navLien">
+                            <?= TITRE_ACTIVITE ?>
+                        </p>
                     </a>
                 </li>
                 <li>
                     <a class="nav-link" href="#anchorBodyRestaurants">
-                    <p class="navLien"><?= TITRE_RESTAURANT ?></p>
+                        <p class="navLien">
+                            <?= TITRE_RESTAURANT ?>
+                        </p>
                     </a>
                 </li>
                 <li>
                     <a class="nav-link" href="#anchorBodyCommentaires">
-                    <p class="navLien"><?= TITRE_COMMENTAIRE ?></p>
+                        <p class="navLien">
+                            <?= TITRE_COMMENTAIRE ?>
+                        </p>
                     </a>
                 </li>
             </ul>
@@ -44,15 +54,17 @@ require_once(PATH_VIEWS.'spinner.php');?>
     </div>
 
     <!--  Les catégories  -->
-     <div class="bodyQuartier">
+    <div class="bodyQuartier col-lg-10 offset-lg-2 col-md-10 offset-md-2 col-sm-12">
         <span class="anchor" id="anchorBodyHistoire"></span>
         <div class="row">
-            <div class="col-lg-4 offset-lg-2 col-md-3 offset-md-2 col-sm-12" id="BodyHistoire">
-                <h1><?= TITRE_HISTOIRE ?></h1>
+            <h1>
+                <?= TITRE_HISTOIRE ?>
+            </h1>
+            <div id="BodyHistoire">
                 <div id="sousTitreHistory"></div>
                 <p id="txtHistory" class="text-justify"></p>
             </div>
-            <div class="col-lg-6 col-md-7 col-sm-12">
+            <div>
                 <!--  Catégorie Histoire  -->
                 <div id="timelineHistory">
                 </div>
@@ -60,8 +72,10 @@ require_once(PATH_VIEWS.'spinner.php');?>
         </div>
         <span class="anchor" id="anchorBodyMonuments"></span>
         <div class="row">
-            <div class="col-lg-10 offset-lg-2 col-md-10 offset-md-2 col-sm-12" id="BodyMonuments">
-                <h1><?= TITRE_MONUMENT ?></h1>
+            <div id="BodyMonuments">
+                <h1>
+                    <?= TITRE_MONUMENT ?>
+                </h1>
                 <div class="carrousel">
                     <div class="arrow-carrousel">
                         <div class="arrow" id="arrowLeftMonument"></div>
@@ -75,91 +89,95 @@ require_once(PATH_VIEWS.'spinner.php');?>
 
         <span class="anchor" id="anchorBodyRestaurants"></span>
         <div class="row">
-            <div class="col-lg-10 offset-lg-2 col-md-10 offset-md-2 col-sm-12" id="bodyRestaurants">
-                <h1><?= TITRE_RESTAURANT ?></h1>
+            <div id="bodyRestaurants">
+                <h1>
+                    <?= TITRE_RESTAURANT ?>
+                </h1>
                 <div class="containerCard" id="RestaurantCards">
 
                 </div>
-                <div class="arrow-restaurant"id="ArrowRes">
+                <div class="arrow-restaurant" id="ArrowRes">
                     <a class="arrow" id="arrowTopRestaurant" href="#anchorBodyRestaurants"></a>
                     <a class="arrow" id="arrowDownRestaurant"></a>
                 </div>
             </div>
         </div>
-    </div> 
         <span class="anchor" id="anchorBodyActivites"></span>
         <div id="bodyActivites" class="row">
-        <h1 class="row col-12"> Activités</h1>
+            <h1 class="row col-12"> Activités</h1>
             <div class="row col-6">
                 <span class="row">fleche</span>
                 <div class="row">
                     <div>
                         <h2>Title 1</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip
-                        ex ea commodo consequat.
-                        Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip
-                        ex ea commodo consequat.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat.
+                            Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat.</p>
                     </div>
                     <div>
                         <h2>Title 2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip
-                        ex ea commodo consequat.
-                        Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip
-                        ex ea commodo consequat.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat.
+                            Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat.</p>
                     </div>
                 </div>
                 <span class="row">fleche</span>
             </div>
         </div>
-    
+
         <span class="anchor" id="anchorBodyCommentaires"></span>
         <div id="bodyCommentaires">
-        <h1> Commentaires</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat.
-            Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat.</p>
+            <h1> Commentaires</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat.</p>
             <?php
             if (empty($_SESSION['user'])){
                 ?>
-                <div>
-                    <form action="" name="formCommentaireBellecour" id="formCommentaireBellecour" method="post">
-                        <div class="form-group">
-                            <label for="nomCommentaire"><?= TXT_COMM_NOM ?></label>
-                            <input class="form-control" type="text" name="nomCommentaire" id="nomCommentaire" value="<?= (isset($_POST['titreTopic'])) ? $_POST['titreTopic'] : "" ?>"/>
-                        </div>
-                        <div class="form-group">
-                            <textarea name="commentaire" id="commentaire" class="form-control" placeholder="<?= TXT_COMM_COMM ?>"></textarea>
-                        </div>
-                        <button type="submit" form="formCommentaireBellecour" name="valFormCommentaireBellecour" class="btn btn-primary"><?= TXT_ENVOYER ?></button>
-                    </form>
-                </div>
-                <?php
+            <div>
+                <form action="" name="formCommentaireBellecour" id="formCommentaireBellecour" method="post">
+                    <div class="form-group">
+                        <label for="nomCommentaire">
+                            <?= TXT_COMM_NOM ?></label>
+                        <input class="form-control" type="text" name="nomCommentaire" id="nomCommentaire" value="<?= (isset($_POST['titreTopic'])) ? $_POST['titreTopic'] : "" ?>" />
+                    </div>
+                    <div class="form-group">
+                        <textarea name="commentaire" id="commentaire" class="form-control" placeholder="<?= TXT_COMM_COMM ?>"></textarea>
+                    </div>
+                    <button type="submit" form="formCommentaireBellecour" name="valFormCommentaireBellecour" class="btn btn-primary">
+                        <?= TXT_ENVOYER ?></button>
+                </form>
+            </div>
+            <?php
             }
             ?>
         </div>
     </div>
+</div>
 </div>
 <script type="text/javascript" src="<?= PATH_SCRIPTS ?>cardMapFactory.js"></script>
 <script type="text/javascript" src="<?= PATH_SCRIPTS ?>HistoryPointFactory.js"></script>
