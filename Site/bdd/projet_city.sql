c05c5262602946d8f928127eda059f08e28ec331
Paul <paulpoupet@atilog.com>
2018-11-18 20:00:43 +0100

    maj bdd

--- Site/bdd/projet_city.sql
+++ Site/bdd/projet_city.sql
@@ -1,5 +1,34 @@
-DROP TABLE IF EXISTS `activite`;
-CREATE TABLE IF NOT EXISTS `activite` (
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le :  Dim 18 nov. 2018 à 19:53
+-- Version du serveur :  10.1.36-MariaDB
+-- Version de PHP :  7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `projet_city`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `activite`
+--
+
+CREATE TABLE `activite` (
   `codeActivite` int(5) NOT NULL,
   `codePays` int(5) DEFAULT NULL,
   `codeQuartier` int(5) DEFAULT NULL,
@@ -8,57 +37,84 @@ CREATE TABLE IF NOT EXISTS `activite` (
   `nomLieux` varchar(50) DEFAULT NULL,
   `coordonnees` point NOT NULL,
   `imageActivite` varchar(50) DEFAULT NULL,
-  `commentaire` text,
-  KEY `codePays` (`codePays`),
-  KEY `codeQuartier` (`codeQuartier`),
-  KEY `codeCategorie` (`codeCategorie`),
-  SPATIAL KEY `coordonnees` (`coordonnees`)
+  `commentaire` text
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `categorie`;
-CREATE TABLE IF NOT EXISTS `categorie` (
+--
+-- Déchargement des données de la table `activite`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
   `codeCategorie` int(5) NOT NULL,
-  `libelleCategorie` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`codeCategorie`),
-  KEY `codeCategorie` (`codeCategorie`)
+  `libelleCategorie` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `commentaire`;
-CREATE TABLE IF NOT EXISTS `commentaire` (
+--
+-- Déchargement des données de la table `categorie`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaire`
+--
+
+CREATE TABLE `commentaire` (
   `codeCommentaire` int(5) NOT NULL,
   `codePays` int(5) DEFAULT NULL,
   `codeQuartier` int(5) DEFAULT NULL,
+  `nom` varchar(50) DEFAULT NULL,
   `commentaire` text,
   `date` date DEFAULT NULL,
-  `actif` int(5) NOT NULL,
-  PRIMARY KEY (`codeCommentaire`),
-  KEY `codeCommentaire` (`codeCommentaire`),
-  KEY `codePays` (`codePays`),
-  KEY `codeQuartier` (`codeQuartier`)
+  `actif` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `etatTopic`;
-CREATE TABLE IF NOT EXISTS `etatTopic` (
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etatTopic`
+--
+
+CREATE TABLE `etatTopic` (
   `codeEtat` int(5) NOT NULL,
-  `libelleEtat` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`codeEtat`),
-  KEY `codeEtat` (`codeEtat`)
+  `libelleEtat` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `histoire`;
-CREATE TABLE IF NOT EXISTS `histoire` (
+--
+-- Déchargement des données de la table `etatTopic`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `histoire`
+--
+
+CREATE TABLE `histoire` (
   `codeHistoire` int(5) NOT NULL,
   `codePays` int(5) NOT NULL,
   `codeQuartier` int(5) NOT NULL,
-  `commentaire` text NOT NULL,
-  PRIMARY KEY (`codeHistoire`),
-  KEY `codeHistoire` (`codeHistoire`),
-  KEY `codePays` (`codePays`),
-  KEY `codeQuartier` (`codeQuartier`)
+  `titre` varchar(50) DEFAULT NULL,
+  `imageHistoire` varchar(50) DEFAULT NULL,
+  `commentaire` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `message`;
-CREATE TABLE IF NOT EXISTS `message` (
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE `message` (
   `codeMessage` int(5) NOT NULL,
   `codeTopic` int(5) DEFAULT NULL,
   `nom` varchar(50) NOT NULL,
@@ -67,8 +123,17 @@ CREATE TABLE IF NOT EXISTS `message` (
   `codeProfile` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `messagecontact`;
-CREATE TABLE IF NOT EXISTS `messagecontact` (
+--
+-- Déchargement des données de la table `message`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messagecontact`
+--
+
+CREATE TABLE `messagecontact` (
   `codeMessage` int(5) NOT NULL,
   `nom` varchar(50) DEFAULT NULL,
   `prenom` varchar(50) DEFAULT NULL,
@@ -78,8 +143,13 @@ CREATE TABLE IF NOT EXISTS `messagecontact` (
   `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `monument`;
-CREATE TABLE IF NOT EXISTS `monument` (
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `monument`
+--
+
+CREATE TABLE `monument` (
   `codeMonument` int(5) NOT NULL,
   `codePays` int(5) DEFAULT NULL,
   `codeQuartier` int(5) DEFAULT NULL,
@@ -88,40 +158,71 @@ CREATE TABLE IF NOT EXISTS `monument` (
   `coordonnees` point NOT NULL,
   `dateConstruction` date DEFAULT NULL,
   `architecte` varchar(50) DEFAULT NULL,
-  `commentaire` text,
-  PRIMARY KEY (`codeMonument`),
-  KEY `codeMonument` (`codeMonument`),
-  KEY `codePays` (`codePays`),
-  KEY `codeQuartier` (`codeQuartier`),
-  SPATIAL KEY `coordonnees` (`coordonnees`)
+  `commentaire` text
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `pays`;
-CREATE TABLE IF NOT EXISTS `pays` (
+--
+-- Déchargement des données de la table `monument`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pays`
+--
+
+CREATE TABLE `pays` (
   `codePays` int(5) NOT NULL,
   `libellePays` varchar(50) DEFAULT NULL,
   `libellePaysCourt` varchar(3) DEFAULT NULL,
-  `fichier` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`codePays`),
-  KEY `codePays` (`codePays`)
+  `fichier` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `profile`;
+--
+-- Déchargement des données de la table `pays`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `profile`
+--
+
 CREATE TABLE `profile` (
   `codeProfile` int(5) NOT NULL,
   `libelleProfile` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `quartier`;
-CREATE TABLE IF NOT EXISTS `quartier` (
+--
+-- Déchargement des données de la table `profile`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `quartier`
+--
+
+CREATE TABLE `quartier` (
   `codeQuartier` int(5) NOT NULL,
-  `libelleQuartier` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`codeQuartier`),
-  KEY `codeQuartier` (`codeQuartier`)
+  `libelleQuartier` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `restaurant`;
-CREATE TABLE IF NOT EXISTS `restaurant` (
+--
+-- Déchargement des données de la table `quartier`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `restaurant`
+--
+
+CREATE TABLE `restaurant` (
   `codeRestaurant` int(5) NOT NULL,
   `codePays` int(5) DEFAULT NULL,
   `codeQuartier` int(5) DEFAULT NULL,
@@ -130,27 +231,35 @@ CREATE TABLE IF NOT EXISTS `restaurant` (
   `numeroTelephone` varchar(20) DEFAULT NULL,
   `coordonnees` point NOT NULL,
   `imageRestaurant` varchar(50) DEFAULT NULL,
-  `commentaire` text,
-  KEY `codePays` (`codePays`),
-  KEY `codeQuartier` (`codeQuartier`),
-  SPATIAL KEY `coordonnees` (`coordonnees`)
+  `commentaire` text
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `topic`;
-CREATE TABLE IF NOT EXISTS `topic` (
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `topic`
+--
+
+CREATE TABLE `topic` (
   `codeTopic` int(5) NOT NULL,
   `codePays` int(5) DEFAULT NULL,
   `libelleTopic` varchar(50) DEFAULT NULL,
   `description` text,
   `codeEtat` int(5) DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  PRIMARY KEY (`codeTopic`),
-  KEY `codeTopic` (`codeTopic`),
-  KEY `codePays` (`codePays`),
-  KEY `codeEtat` (`codeEtat`)
+  `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `user`;
+--
+-- Déchargement des données de la table `topic`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
 CREATE TABLE `user` (
   `codeUser` int(5) NOT NULL,
   `nom` varchar(50) DEFAULT NULL,
@@ -160,46 +269,160 @@ CREATE TABLE `user` (
   `codeProfile` int(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `user`
+--
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `activite`
+--
 ALTER TABLE `activite`
-  ADD CONSTRAINT `activite_ibfk_1` FOREIGN KEY (`codePays`) REFERENCES `pays` (`codePays`),
-  ADD CONSTRAINT `activite_ibfk_2` FOREIGN KEY (`codeQuartier`) REFERENCES `quartier` (`codeQuartier`),
-  ADD CONSTRAINT `activite_ibfk_3` FOREIGN KEY (`codeCategorie`) REFERENCES `categorie` (`codeCategorie`);
+  ADD KEY `codePays` (`codePays`),
+  ADD KEY `codeQuartier` (`codeQuartier`),
+  ADD KEY `codeCategorie` (`codeCategorie`),
+  ADD SPATIAL KEY `coordonnees` (`coordonnees`);
 
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`codeCategorie`),
+  ADD KEY `codeCategorie` (`codeCategorie`);
+
+--
+-- Index pour la table `commentaire`
+--
 ALTER TABLE `commentaire`
-  ADD CONSTRAINT `commentaire_ibfk_1` FOREIGN KEY (`codePays`) REFERENCES `pays` (`codePays`),
-  ADD CONSTRAINT `commentaire_ibfk_2` FOREIGN KEY (`codeQuartier`) REFERENCES `quartier` (`codeQuartier`);
+  ADD PRIMARY KEY (`codeCommentaire`),
+  ADD KEY `codeCommentaire` (`codeCommentaire`),
+  ADD KEY `codePays` (`codePays`),
+  ADD KEY `codeQuartier` (`codeQuartier`);
 
+--
+-- Index pour la table `etatTopic`
+--
+ALTER TABLE `etatTopic`
+  ADD PRIMARY KEY (`codeEtat`),
+  ADD KEY `codeEtat` (`codeEtat`);
+
+--
+-- Index pour la table `histoire`
+--
 ALTER TABLE `histoire`
-  ADD CONSTRAINT `histoire_ibfk_1` FOREIGN KEY (`codePays`) REFERENCES `pays` (`codePays`),
-  ADD CONSTRAINT `histoire_ibfk_2` FOREIGN KEY (`codeQuartier`) REFERENCES `quartier` (`codeQuartier`);
+  ADD PRIMARY KEY (`codeHistoire`),
+  ADD KEY `codeHistoire` (`codeHistoire`),
+  ADD KEY `codePays` (`codePays`),
+  ADD KEY `codeQuartier` (`codeQuartier`);
 
+--
+-- Index pour la table `message`
+--
 ALTER TABLE `message`
-  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`codeTopic`) REFERENCES `topic` (`codeTopic`),
-  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`codeProfile`) REFERENCES `profile` (`codeProfile`);
   ADD PRIMARY KEY (`codeMessage`),
   ADD KEY `codeMessage` (`codeMessage`),
   ADD KEY `codeTopic` (`codeTopic`),
   ADD KEY `codeProfile` (`codeProfile`);
 
-ALTER TABLE `message`
-  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`codeTopic`) REFERENCES `topic` (`codeTopic`);
-  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`codeProfile`) REFERENCES `profile` (`codeProfile`);
+--
+-- Index pour la table `monument`
+--
+ALTER TABLE `monument`
+  ADD PRIMARY KEY (`codeMonument`),
+  ADD KEY `codeMonument` (`codeMonument`),
+  ADD KEY `codePays` (`codePays`),
+  ADD KEY `codeQuartier` (`codeQuartier`),
+  ADD SPATIAL KEY `coordonnees` (`coordonnees`);
+
+--
+-- Index pour la table `pays`
+--
+ALTER TABLE `pays`
+  ADD PRIMARY KEY (`codePays`),
+  ADD KEY `codePays` (`codePays`);
+
+--
+-- Index pour la table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`codeProfile`),
   ADD KEY `codeProfile` (`codeProfile`);
 
-ALTER TABLE `monument`
-  ADD CONSTRAINT `monument_ibfk_1` FOREIGN KEY (`codePays`) REFERENCES `pays` (`codePays`),
-  ADD CONSTRAINT `monument_ibfk_2` FOREIGN KEY (`codeQuartier`) REFERENCES `quartier` (`codeQuartier`);
+--
+-- Index pour la table `quartier`
+--
+ALTER TABLE `quartier`
+  ADD PRIMARY KEY (`codeQuartier`),
+  ADD KEY `codeQuartier` (`codeQuartier`);
 
+--
+-- Index pour la table `restaurant`
+--
 ALTER TABLE `restaurant`
-  ADD CONSTRAINT `restaurant_ibfk_1` FOREIGN KEY (`codePays`) REFERENCES `pays` (`codePays`),
-  ADD CONSTRAINT `restaurant_ibfk_2` FOREIGN KEY (`codeQuartier`) REFERENCES `quartier` (`codeQuartier`);
+  ADD KEY `codePays` (`codePays`),
+  ADD KEY `codeQuartier` (`codeQuartier`),
+  ADD SPATIAL KEY `coordonnees` (`coordonnees`);
 
+--
+-- Index pour la table `topic`
+--
 ALTER TABLE `topic`
-  ADD CONSTRAINT `topic_ibfk_1` FOREIGN KEY (`codePays`) REFERENCES `pays` (`codePays`),
-  ADD CONSTRAINT `topic_ibfk_2` FOREIGN KEY (`codeEtat`) REFERENCES `etattopic` (`codeEtat`);
+  ADD PRIMARY KEY (`codeTopic`),
+  ADD KEY `codeTopic` (`codeTopic`),
+  ADD KEY `codePays` (`codePays`),
+  ADD KEY `codeEtat` (`codeEtat`);
 
+--
+-- Index pour la table `user`
+--
 ALTER TABLE `user`
   ADD PRIMARY KEY (`codeUser`),
   ADD UNIQUE KEY `login` (`login`),
   ADD KEY `codeUser` (`codeUser`),
   ADD KEY `codeProfile` (`codeProfile`);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD CONSTRAINT `commentaire_ibfk_1` FOREIGN KEY (`codePays`) REFERENCES `pays` (`codePays`),
+  ADD CONSTRAINT `commentaire_ibfk_2` FOREIGN KEY (`codeQuartier`) REFERENCES `quartier` (`codeQuartier`);
+
+--
+-- Contraintes pour la table `histoire`
+--
+ALTER TABLE `histoire`
+  ADD CONSTRAINT `histoire_ibfk_1` FOREIGN KEY (`codePays`) REFERENCES `pays` (`codePays`),
+  ADD CONSTRAINT `histoire_ibfk_2` FOREIGN KEY (`codeQuartier`) REFERENCES `quartier` (`codeQuartier`);
+
+--
+-- Contraintes pour la table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`codeTopic`) REFERENCES `topic` (`codeTopic`),
+  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`codeProfile`) REFERENCES `profile` (`codeProfile`);
+
+--
+-- Contraintes pour la table `topic`
+--
+ALTER TABLE `topic`
+  ADD CONSTRAINT `topic_ibfk_1` FOREIGN KEY (`codePays`) REFERENCES `pays` (`codePays`),
+  ADD CONSTRAINT `topic_ibfk_2` FOREIGN KEY (`codeEtat`) REFERENCES `etattopic` (`codeEtat`);
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`codeProfile`) REFERENCES `profile` (`codeProfile`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
