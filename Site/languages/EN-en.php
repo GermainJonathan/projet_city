3c74902f0a0da0f8b13a671be911550396e8a8a4
gilliane71 <gilliane.brule@gmail.com>
2018-11-29 19:31:29 +0100

    fichier pour traduction anglais créé

--- Site/languages/EN-en.php
+++ Site/languages/EN-en.php
@@ -2,58 +2,56 @@
 
 //Les textes affichés sur le site, sous forme de constantes
 //Les constantes se comprennent sans commentaires. 
-
-//Version Française
+// Version Anglaise
 
 // titre de page
-define('MENU_ACCUEIL','Accueil');
+define('MENU_ACCUEIL','Home');
 define('MENU_TERREAUX', 'Terreaux');
 define('MENU_PERRACHE', 'Perrache');
 define('MENU_BELLECOUR', 'Bellecour');
 define('MENU_CONTACT', 'Contact');
 define('MENU_FORUM', 'Forum');
 define('MENU_ADMINISTRATION', 'Administration');
-define('MENU_DECONNEXION', 'Déconnexion');
+define('MENU_DECONNEXION', 'Sign out');
 
 define('TEXTE_PAGE_404','Oops, la page demandée n\'existe pas !');
 define('MESSAGE_ERREUR',"Une erreur s'est produite");
 
 define('TITRE', 'Presqu\'île');
-define('TITRE_HISTOIRE', 'Histoire');
+define('TITRE_HISTOIRE', 'History');
 define('TITRE_MONUMENT', 'Monuments');
-define('TITRE_ACTIVITE', 'Activités');
+define('TITRE_ACTIVITE', 'Activities');
 define('TITRE_RESTAURANT', 'Restaurants');
-define('TITRE_COMMENTAIRE', 'Commentaire');
+define('TITRE_COMMENTAIRE', 'Comments');
 define('TITRE_MENU', 'Menu');
 define('TITRE_DATE', 'Date de parution');
-define('TITRE_COMM_NOM', 'Nom');
-define('TITRE_TOPIC', 'Titre du Topic');
-define('TITRE_ETATA_TOPIC', 'Etat du topic');
+define('TITRE_COMM_NOM', 'Name');
+define('TITRE_TOPIC', 'Title of the topic');
+define('TITRE_ETATA_TOPIC', 'State of the topic');
 define('TITRE_MESSAGE', 'Message');
 define('TITRE_ACTION', 'Action');
 define('TITRE_ADMINISTRATION', 'Administration');
-define('TITRE_ADMIN_NOM', 'Nom');
+define('TITRE_ADMIN_NOM', 'Name');
 define('TITRE_ADMIN_MAIL', 'Mail');
 define('TITRE_ADMIN_PROFILE', 'Profile');
-define('TITRE_MENTIONS_LEGALES', 'Mentions Légales');
-define('TITRE_LIENS_PRATIQUES', 'Liens Pratiques');
+define('TITRE_MENTIONS_LEGALES', 'Legal Notice');
+define('TITRE_LIENS_PRATIQUES', 'Practical links');
 
 define('TITRE_PAGE_ACCUEIL','DÉCOUVREZ LA PRESQU\'ÎLE');
 
-define('TXT_FOOTER_DESCRIPTION', 'La Presqu\'île, quartier le plus dynamique de Lyon, découvrez son histoire, sa gastronomie, ses activités    ');
-define('TXT_TITRE_TOPIC', 'Titre du topic');
-define('TXT_TITRE_DESCRIPTION', 'Description du topic');
+define('TXT_TITRE_TOPIC', 'Title of the topic');
+define('TXT_TITRE_DESCRIPTION', 'Description of the topic');
 
-define('TXT_COMM_NOM', 'Nom');
+define('TXT_COMM_NOM', 'Name');
 define('TXT_COMM_COMM', 'Commentaire');
 
 define('TXT_ENVOYER', 'Envoyer');
 
-define('TXT_VALIDER', 'Valider');
-define('TXT_RESOLUE', 'Résolue');
-define('TXT_REFUSER', 'Refuser');
-define('TXT_ANNULER', 'Annuler');
-define('TXT_SUPPRIMER', 'Supprimer');
+define('TXT_VALIDER', 'Validate');
+define('TXT_RESOLUE', 'Resolved');
+define('TXT_REFUSER', 'Refuse');
+define('TXT_ANNULER', 'Cancel');
+define('TXT_SUPPRIMER', 'Remove');
 
 define('ERREUR_TOPC_DSC', 'Il manque la description');
 define('ERREUR_TOPC_TITRE', 'Il manque le titre');
@@ -65,12 +63,12 @@ define('ERREUR_CONN_REFUSEE', 'Accès refusé. Impossible de se connecter avec c
 define('SUCCESS_CREATE_TOPIC', 'Topic en attente de validation par l\'administrateur');
 
 //  Texte pour la page de contact
-define ('TITRE_CONTACT_A_PROPOS'," À propos");
+define ('TITRE_CONTACT_A_PROPOS',"About");
 define('TXT_CONTACT_APROPOS',"Une question, un besoin , une remarque ... ? <br/> Nous sommes à votre disposition et vous répondrons dans les meilleurs délais. N'hésitez pas à nous contacter !");
 define('TXT_CONTACT_FORMULAIRE',"Ou Compléter le formulaire");
 // Champ Formlaire
-define('CHAMP_NOM',"Votre Nom");
-define ("CHAMP_PRENOM","Votre Prénom");
-define("CHAMP_EMAIL","Votre Email");
-define("CHAMP_SUJET","Sujet");
-define("CHAMP_MESSAGE","Votre Message");
+define('CHAMP_NOM',"Your Name");
+define ("CHAMP_PRENOM","Your First Name");
+define("CHAMP_EMAIL","Your Email");
+define("CHAMP_SUJET","Object");
+define("CHAMP_MESSAGE","Yous Message");
