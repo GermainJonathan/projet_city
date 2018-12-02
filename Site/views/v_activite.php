27c1e40b360cc8a62b7d045f879bbd3f3ad02dbf
Julien Giraud-Sauveur <julien.gs@hotmail.fr>
2018-12-02 14:32:36 +0100

    :art: Activité - ajout d'une image et mis en place du responsive

--- Site/views/v_activite.php
+++ Site/views/v_activite.php
@@ -7,11 +7,16 @@
             </h1>
         </div>
         <div class="row">
-            <div class="col-9">
-                <div class="arrow" id="arrowUpActivity"></div>
-                <div id="activiteconteneur" class="row">
+            <div class="row col-xl-8 col-lg-6 col-sm-12 justify-content-md-center">
+                <div class="col-10">
+                    <div class="arrow" id="arrowUpActivity"></div>
+                    <div id="activiteconteneur" class="row">
+                    </div>
+                    <div class="arrow" id="arrowDownActivity"></div>
                 </div>
-                <div class="arrow" id="arrowDownActivity"></div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-sm-12 align-self-center">
+            <img src="assets/images/perrache/Monument - Basilic Saint Martin D'annay.jpg" style="transform: rotate(90deg);" class="img-fluid" alt="Responsive image">
             </div>
         </div>
     </div>
