84b904763212156f6493ccd97bb1409ded0936ea
Bif4sse <43471869+Bif4sse@users.noreply.github.com>
2018-12-03 10:52:30 +0100

    Update EN-en : Traduction complète
    Retraduit l'intégralité du fichier créer et déjà partiellement traduit par Gilliane

--- Site/languages/EN-en.php
+++ Site/languages/EN-en.php
@@ -14,17 +14,17 @@ define('MENU_FORUM', 'Forum');
 define('MENU_ADMINISTRATION', 'Administration');
 define('MENU_DECONNEXION', 'Sign out');
 
-define('TEXTE_PAGE_404','Oops, la page demandée n\'existe pas !');
-define('MESSAGE_ERREUR',"Une erreur s'est produite");
+define('TEXTE_PAGE_404','Oops, the page you are asking for doesn\'t exist !');
+define('MESSAGE_ERREUR',"An error occured");
 
-define('TITRE', 'Presqu\'île');
+define('TITRE', 'Peninsula');
 define('TITRE_HISTOIRE', 'History');
 define('TITRE_MONUMENT', 'Monuments');
 define('TITRE_ACTIVITE', 'Activities');
 define('TITRE_RESTAURANT', 'Restaurants');
 define('TITRE_COMMENTAIRE', 'Comments');
 define('TITRE_MENU', 'Menu');
-define('TITRE_DATE', 'Date de parution');
+define('TITRE_DATE', 'Date of publication');
 define('TITRE_COMM_NOM', 'Name');
 define('TITRE_TOPIC', 'Title of the topic');
 define('TITRE_ETATA_TOPIC', 'State of the topic');
@@ -37,15 +37,15 @@ define('TITRE_ADMIN_PROFILE', 'Profile');
 define('TITRE_MENTIONS_LEGALES', 'Legal Notice');
 define('TITRE_LIENS_PRATIQUES', 'Practical links');
 
-define('TITRE_PAGE_ACCUEIL','DÉCOUVREZ LA PRESQU\'ÎLE');
+define('TITRE_PAGE_ACCUEIL','DISCOVER THE PENINSULA');
 
 define('TXT_TITRE_TOPIC', 'Title of the topic');
 define('TXT_TITRE_DESCRIPTION', 'Description of the topic');
 
 define('TXT_COMM_NOM', 'Name');
-define('TXT_COMM_COMM', 'Commentaire');
+define('TXT_COMM_COMM', 'Comments');
 
-define('TXT_ENVOYER', 'Envoyer');
+define('TXT_ENVOYER', 'Send');
 
 define('TXT_VALIDER', 'Validate');
 define('TXT_RESOLUE', 'Resolved');
@@ -53,22 +53,22 @@ define('TXT_REFUSER', 'Refuse');
 define('TXT_ANNULER', 'Cancel');
 define('TXT_SUPPRIMER', 'Remove');
 
-define('ERREUR_TOPC_DSC', 'Il manque la description');
-define('ERREUR_TOPC_TITRE', 'Il manque le titre');
+define('ERREUR_TOPC_DSC', 'The description is missing');
+define('ERREUR_TOPC_TITRE', 'The title is missing');
 
-define('ERREUR_CONN_LOGIN_VOID', 'Il manque le login');
-define('ERREUR_CONN_PW_VOID', 'Il manque le mot de passe');
-define('ERREUR_CONN_REFUSEE', 'Accès refusé. Impossible de se connecter avec ces informations');
+define('ERREUR_CONN_LOGIN_VOID', 'The login is missing');
+define('ERREUR_CONN_PW_VOID', 'The password is missing');
+define('ERREUR_CONN_REFUSEE', 'Access denied. Can\'t connect with these informations');
 
-define('SUCCESS_CREATE_TOPIC', 'Topic en attente de validation par l\'administrateur');
+define('SUCCESS_CREATE_TOPIC', 'Topic waiting for the administrator aprouval');
 
 //  Texte pour la page de contact
 define ('TITRE_CONTACT_A_PROPOS',"About");
-define('TXT_CONTACT_APROPOS',"Une question, un besoin , une remarque ... ? <br/> Nous sommes à votre disposition et vous répondrons dans les meilleurs délais. N'hésitez pas à nous contacter !");
-define('TXT_CONTACT_FORMULAIRE',"Ou Compléter le formulaire");
+define('TXT_CONTACT_APROPOS',"A question, a need, an observation ... ? <br/> we remain available and will be answering you as soon as possible. Don't hesitate to contact us !");
+define('TXT_CONTACT_FORMULAIRE',"Or complete the form");
 // Champ Formlaire
 define('CHAMP_NOM',"Your Name");
 define ("CHAMP_PRENOM","Your First Name");
 define("CHAMP_EMAIL","Your Email");
 define("CHAMP_SUJET","Object");
-define("CHAMP_MESSAGE","Yous Message");
+define("CHAMP_MESSAGE","Your Message");
