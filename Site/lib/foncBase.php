b5660b23861e0df728ec99bc0e9506aa42132605
Paul <paulpoupet@atilog.com>
2018-12-04 15:57:00 +0100

    get user IP

--- Site/lib/foncBase.php
+++ Site/lib/foncBase.php
@@ -153,3 +153,34 @@ function convertCoordonees($arrayToConvert) {
     $chaine[0] = explode(' ', $chaine[0]);
     return array('x' => floatval($chaine[0][0]), 'y' => floatval($chaine[0][1]));
 }
+
+function getUserIP()
+{
+    // Get real visitor IP behind CloudFlare network
+    if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+        $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+        $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    }
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    echo $client . "<br>";
+    echo $forward . "<br>";
+    echo $remote . "<br>";
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
